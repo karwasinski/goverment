af33457493e6b5138c0435b57bd5016eaedd628b
karwasinski <karjas@poczta.fm>
2014-12-29 16:34:15 +0100

    poprawki wstepne, bardzo wstepne

--- pl.gov.pkw/src/main/java/hibernate/HibernateTest.java
+++ pl.gov.pkw/src/main/java/hibernate/HibernateTest.java
@@ -2,6 +2,8 @@ package hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 import db.dto.UserDetails;
 
@@ -11,12 +13,20 @@ public class  HibernateTest {
 	user.setUserId(1);
 	user.setUserName("Jan Nowak");
 	
-	//@SuppressWarnings("deprecation")
-	SessionFactory sesionFactory = new Configuration().configure().buildSessionFactory();
-	Session session = sesionFactory.openSession();
+	Configuration configuration = new Configuration();
+	configuration.configure("hibernate.cfg.xml");
+	System.out.println("Hibernate Configuration loaded");
+	
+	ServiceRegistry sr = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+	System.out.println("Hibernate serviceRegistry created");
+	
+	SessionFactory sessionFactory = configuration.buildSessionFactory(sr);
+	Session session = sessionFactory.getCurrentSession();//openSession();
+	session = sessionFactory.openSession();
 	session.beginTransaction();
 	session.save(user);
 	session.getTransaction().commit();
+	session.close();
 	
 	}
 	
